bf5e08a6bc9855d2b365ef0218fb0be433aab9ae
Daniel Ali <daniel.ray.ali@gmail.com>
2020-04-22 21:28:04 -0400

    Saving A20 enable implementation

--- kernel/cpu.cpp
+++ kernel/cpu.cpp
@@ -59,48 +59,49 @@ asm (
 );
 
 asm (
-"; Function: check_a20"
-";"
-"; Purpose: to check the status of the a20 line in a completely self-contained state-preserving way."
-";          The function can be modified as necessary by removing push's at the beginning and their"
-";          respective pop's at the end if complete self-containment is not required."
-";"
-"; Returns: 0 in ax if the a20 line is disabled (memory wraps around)"
-";          1 in ax if the a20 line is enabled (memory does not wrap around)"
-"CheckA20:"
-    "pushf"
-    "push ds"
-    "push es"
-    "push di"
-    "push si"
-    "cli"
-    "xor ax, ax ; ax = 0"
-    "mov es, ax"
-    "not ax ; ax = 0xFFFF"
-    "mov ds, ax"
-    "mov di, 0x0500"
-    "mov si, 0x0510"
-    "mov al, byte [es:di]"
-    "push ax"
-    "mov al, byte [ds:si]"
-    "push ax"
-    "mov byte [es:di], 0x00"
-    "mov byte [ds:si], 0xFF"
-    "cmp byte [es:di], 0xFF"
-    "pop ax"
-    "mov byte [ds:si], al"
-    "pop ax"
-    "mov byte [es:di], al"
-    "mov ax, 0"
-    "je check_a20__exit"
-    "mov ax, 1"
-"check_a20__exit:"
-    "pop si"
-    "pop di"
-    "pop es"
-    "pop ds"
-    "popf"
-    "ret"
+"# Function: check_a20"
+"#"
+"# Purpose: to check the status of the a20 line in a completely self-contained state-preserving way."
+"#          The function can be modified as necessary by removing push's at the beginning and their"
+"#          respective pop's at the end if complete self-containment is not required."
+"#"
+"# Returns: 0 in ax if the a20 line is disabled (memory wraps around)"
+"#          1 in ax if the a20 line is enabled (memory does not wrap around)"
+".global CheckA20\n"
+"CheckA20:\n"
+    "pushf\n"
+    "push %ds\n"
+    "push %es\n"
+    "push %di\n"
+    "push %si\n"
+    "cli\n"
+    "xor %ax, %ax # ax = 0\n"
+    "mov %ax, %es\n"
+    "not %ax # ax = 0xFFFF\n"
+    "mov %ax, %ds\n"
+    "mov $0x0500, %di\n"
+    "mov $0x0510, %si\n"
+    "movb %es:(%di), al\n"
+    "push ax\n"
+    "mov al, byte [ds:si]\n"
+    "push ax\n"
+    "mov byte [es:di], 0x00\n"
+    "mov byte [ds:si], 0xFF\n"
+    "cmp byte [es:di], 0xFF\n"
+    "pop ax\n"
+    "mov byte [ds:si], al\n"
+    "pop ax\n"
+    "mov byte [es:di], al\n"
+    "mov ax, 0\n"
+    "je check_a20__exit\n"
+    "mov ax, 1\n"
+"check_a20__exit:\n"
+    "pop si\n"
+    "pop di\n"
+    "pop es\n"
+    "pop ds\n"
+    "popf\n"
+    "ret\n"
 );
 
 bool CPU::Initialize() {
@@ -134,7 +135,7 @@ bool CPU::Initialize() {
     }
 
     // Disable non-maskable interrupts and maskable interrupts
-    IO::OutByte(0x70, IO::InByte(0x70) | 0x80);
+    IO::Out(0x70, IO::In(0x70) | 0x80);
     asm ("cli\n");
 
     // Check if A20 memory line has been enabled
@@ -144,11 +145,13 @@ bool CPU::Initialize() {
         KeyboardController::InitializeA20();
     }
 
-    // Enter protected mode
-    asm (
-        "cli\n"
-        "lgdt (%gdtr)\n"
-    );
+    Log("A20 memory line has been enabled\n");
+
+    // // Enter protected mode
+    // asm (
+    //     "cli\n"
+    //     "lgdt (%gdtr)\n"
+    // );
 
     return false;
 }
